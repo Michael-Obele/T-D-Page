d1ee41e15faf39b3f252e165fd6728baef7251bc
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2021-01-19 09:47:57 +0100

    finally fixed button

--- js.js
+++ js.js
@@ -1,43 +1,42 @@
 function size() {
-  if(window.innerWidth < 768) {
+  if (window.innerWidth < 768) {
     var x = document.getElementById("hide");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
-    }; 
- }
- else {
-  document.getElementById("hide").style.display = "block";
- };
+    };
+  }
+  else {
+    document.getElementById("hide").style.display = "block";
+  };
 };
-window.onresize = function(){if(window.innerWidth < 768) {
-  var x = document.getElementById("hide");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }; 
+window.onresize = function () {
+  if (window.innerWidth < 768) {
+    var x = document.getElementById("hide");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    };
+  }
+  else {
+    document.getElementById("hide").style.display = "block";
+  };
 }
-else {
-document.getElementById("hide").style.display = "block";
+
+
+function blured() {
+  if (window.innerWidth < 768) {
+    document.getElementById("hide").style.display = "none";
+  }
+  else {
+    document.getElementById("hide").style.display = "block";
+  };
 };
-}
 
 
-  function blured() {
-    if (window.innerWidth < 768) {
-      var x = document.getElementById("hide");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }; 
-      };
-    }
 
-
-    
 // Old method
 // window.onresize = function(){
 //   document.getElementById("hide").style.display = "block";
